86ba77cb6affedbe42aec334246f31afb6152dc3
APIVerve Team <hello@apiverve.com>
2025-02-03 15:40:48 -0600

    Minor Version Update

---
+++ HardinessZoneLookupQueryOptions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class HardinessZoneLookupQueryOptions {
+public string zip { get; set; }
+}
+
+}
