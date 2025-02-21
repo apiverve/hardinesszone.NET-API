24527b1afa38da98c151dc773f29d1fd77283f9e
APIVerve Team <hello@apiverve.com>
2025-02-21 07:36:14 -0600

    Added new API Endpoints

---
+++ USDAHardinessZoneQueryOptions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class USDAHardinessZoneQueryOptions {
+public string zip { get; set; }
+}
+
+}
